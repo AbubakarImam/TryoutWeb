2a032ce60a7eb1588c8367163ca0d7abc6f2dcb0
Abubakar Imam <salamabubakarimam@gmail.com>
2025-09-23 13:39:03 +0100

    ShoppingCart

---
+++ Tryout.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tryout.DataAccess.Data;
+using Tryout.DataAccess.Repository.IRepository;
+using Tryout.Models;
+
+namespace Tryout.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+    }
+}
--- Tryout/Areas/Customer/Controllers/HomeController.cs
+++ Tryout/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Tryout.DataAccess.Repository.IRepository;
 using Tryout.Models;
@@ -24,8 +26,43 @@ namespace Tryout.Areas.Customer.Controllers
         }
         public IActionResult Details(int productId)
         {
-            Product product = _unitOfWork.Product.Get(u => u.Id==productId, includeProperties: "Category");
-            return View(product);
+            ShoppingCart cart = new ()
+            {
+                ProductId = productId,
+                Count = 1,
+                Product =  _unitOfWork.Product.Get(u => u.Id==productId, includeProperties: "Category")
+            };
+            
+            return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId &&
+            u.ProductId == shoppingCart.ProductId);
+
+            if (cartFromDb != null)
+            {
+                //Shopping cart exist
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            else
+            {
+                //add cart record
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+
+            }
+            TempData["success"] = "Cart updated Successfully";
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
